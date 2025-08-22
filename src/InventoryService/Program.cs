275354b2ba4f860897856a669fbc7f1b196260cf
Hans Løken <hans.abel.loken@fhi.no>
2025-08-22 12:55:37 +0200

    Replaced skeleton with proper implementation for InventoryService, also updates http-file

--- src/InventoryService/Program.cs
+++ src/InventoryService/Program.cs
@@ -1,3 +1,16 @@
+using InventoryService;
+using InventoryService.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+var inventoryRepository = new InventoryRepository
+{
+    Items = new()
+    {
+        { "ice-cream", 3 },
+        { "banana", 1 }
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire components.
@@ -6,34 +19,48 @@ builder.AddServiceDefaults();
 // Add services to the container.
 builder.Services.AddProblemDetails();
 
+// Add inventory
+builder.Services.AddSingleton<InventoryRepository>(inventoryRepository);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapPost("/reserve-items", ([FromBody] ReserveItemsRequest request, InventoryRepository inventory, ILogger<Program> logger) =>
+    {
+        using var scope = logger.BeginScope(new Dictionary<string, object> {["OrderId"] = request.OrderId});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-        .ToArray();
-    return forecast;
-});
+        return ReserveItem(request, inventory, logger) switch
+        {
+            true => Results.Ok(new ReserveItemsResponse {Success = true}),
+            _ => Results.BadRequest(new ReserveItemsResponse {Success = false, ErrorMessage = "Out of stock"})
+        };
+    }
+);
+
+app.MapGet("", (InventoryRepository inventory) =>
+    new GetAllResponse(inventory.Items.Select(item => new GetAllResponseItem(item.Key, item.Value)).ToArray())
+);
 
 app.MapDefaultEndpoints();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+bool ReserveItem(ReserveItemsRequest request, InventoryRepository inventory, ILogger<Program> logger)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    var success = inventory.ReserveItems(request.ItemName, request.NumberOfItems);
+
+    if (success)
+    {
+        logger.LogInformation("Successfully reserved from inventory for order: {OrderId} item: {ItemName} quantity: {NumberOfItems}",
+            request.OrderId, request.ItemName, request.NumberOfItems);
+
+        return true;
+    }
+
+    logger.LogWarning("Could not reserve from inventory for order: {OrderId} item: {ItemName} quantity: {NumberOfItems}",
+        request.OrderId, request.ItemName, request.NumberOfItems);
+
+    return false;
 }
